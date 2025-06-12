79b9bfc3a825c8f8686dfa677d8319b39fd14aa3
kashif9911 <kashif9911@hotmail.com>
2025-06-12 21:33:38 +0500

    debuging

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./src/Home";
-import Experience from "./src/Experience";
-import "./src/index.css";
+import Home from "./Home";
+import Experience from "./Experience";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
